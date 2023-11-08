98f7e0340cbd909d3e24222c1bced25c20951ada
danirus6 <44194174+danirus6@users.noreply.github.com>
2023-11-08 18:51:11 +0100

    Correccion y finalizacion ejercicio+ extra

--- main.js
+++ main.js
@@ -7,96 +7,99 @@ const formSearch = document.getElementById('form');
 const searchInput = document.getElementById('searchId');
 
 
-// const options = {
-//     method: 'GET',
-//     headers: {
-//         accept: 'application/json',
-//         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNmFhN2E5MDBkM2Y2YjA4ZTg0MmY2ZTRhNjQxMjk0OSIsInN1YiI6IjY1NDYxNGUzNmJlYWVhMDEwYjMyZmY0NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MlFipgwXq2B2-suY_9IfNeYwj0a3sR_qbFCLBQD8To0'
-//     }
-// };
-
-const showMovies = (movies) => {
+const showMovies = async (movies) => {
     moviesContainer.innerHTML = '';
-    movies.forEach((movie) => {
+    for (const movie of movies) {
+        const genres = await getGenres(movie.genre_ids);
         moviesContainer.innerHTML += `
-    	<div class="card col-lg-3 col-xs-12 col-md-6 m-2">
-				<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="Card image">
-				<div class="card-body">
-						<h3 class="card-header">${movie.original_title}</h3>
-						<h5 class="card-title">${movie.overview}</h5>
-                        <h5 class="card-title">${getGenres(movie.genre_ids)}</h5 >
-					</div >
-     	</div >
-    `
-    })
+            <div class="card col-lg-3 col-xs-12 col-md-6 m-2">
+                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="Card image">
+                <div class="card-body">
+                    <h3 class="card-header">${movie.original_title}</h3>
+                    <h5 class="card-title">${movie.overview}</h5>
+                    <h5 class="card-title">${genres}</h5>
+                </div>
+            </div>
+        `;
+    }
 }
 
-const getGenres = async (a) => {
-    const result = await axios.get(`https://api.themoviedb.org/3/genre/movie/list?language=en&api_key=${APIKEY}`);
-    console.log(result);
-    const arrayGenresName = [];
-    let cadena;
-    console.log(a);
-    console.log(result.data.genres);
-    result.data.genres.forEach((genres => {
-        try {
-            if (a.includes(genres.id)) {
-                arrayGenresName.push(genres.name);
-                console.log("arrayGeneros: ",arrayGenresName);
-            }
-        } catch (error) {
-            console.log(error);
-        }
-    }))
-    cadena = arrayGenresName.join(',')
-
+const getGenres = async (genreIds) => {
+    try {
+        const result = await axios.get(`https://api.themoviedb.org/3/genre/movie/list?language=en&api_key=${APIKEY}`);
+        const genres = result.data.genres.filter(genre => genreIds.includes(genre.id));
+        const genreNames = genres.map(genre => genre.name).join(', ');
+        return genreNames;
+    } catch (error) {
+        console.log(error);
+        return "Genre not found";
+    }
 }
-
 
 const searchMovies = async (e) => {
     e.preventDefault()
     try {
         const search = searchInput.value;
         const res = await axios.get(`${url}?query=${search}&include_adult=false&page=1&api_key=${APIKEY}`);
-        console.log(res);
         const movies = res.data.results;
-        movies.forEach((movie) => {
-             getGenres(movie.genre_ids)
-        });
-       
         showMovies(movies);
     } catch (error) {
         console.error(error);
     }
 }
 
-formSearch.addEventListener('submit', searchMovies)
+formSearch.addEventListener('submit', searchMovies);
+// const showMovies = (movies) => {
+//     moviesContainer.innerHTML = '';
+//     movies.forEach((movie) => {
+//         moviesContainer.innerHTML += `
+//     	<div class="card col-lg-3 col-xs-12 col-md-6 m-2">
+// 				<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="Card image">
+// 				<div class="card-body">
+// 						<h3 class="card-header">${movie.original_title}</h3>
+// 						<h5 class="card-title">${movie.overview}</h5>
+//                         <h5 class="card-title">${getGenres(movie.genre_ids)}</h5 >
+// 					</div >
+//      	</div >
+//     `
+//     })
+// }
 
+// const getGenres = async (a) => {
+//     const result = await axios.get(`https://api.themoviedb.org/3/genre/movie/list?language=en&api_key=${APIKEY}`);
+//     console.log(result);
+//     const arrayGenresName = [];
+//     let cadena;
+//     console.log(a);
+//     console.log(result.data.genres);
+//     result.data.genres.forEach((genres => {
+//         try {
+//             if (a.includes(genres.id)) {
+//                 arrayGenresName.push(genres.name);
+//                 console.log("arrayGeneros: ",arrayGenresName);
+//             }
+//         } catch (error) {
+//             console.log(error);
+//         }
+//     }))
+//     cadena = arrayGenresName.join(',')
 
-
-// const getData = async () => {
-//     try {
-//         const { data } = await axios.get(
-//             'https://jsonplaceholder.typicode.com/posts'
-//         )
-//         data.forEach((post) => {
-//             postsContainer.innerHTML += `
-// 				<h2>${post.title}</h2>
-// 				<p>${post.body}</p>
-// 				<button onclick=editData(${post.id})>EDIT</button>
-// 				<button onclick=deleteData(${post.id})>DELETE</button>
-// 			`
-//         })
-//     } catch (error) {
-//         console.error(err)
-//     }
 // }
 
 
-// obtenerDatos(url)
-//     .then(datos => {
-//         console.log(datos);
-//     })
-//     .catch(error => {
+// const searchMovies = async (e) => {
+//     e.preventDefault()
+//     try {
+//         const search = searchInput.value;
+//         const res = await axios.get(`${url}?query=${search}&include_adult=false&page=1&api_key=${APIKEY}`);
+//         console.log(res);
+//         const movies = res.data.results;
+//         movies.forEach((movie) => {
+//              getGenres(movie.genre_ids)
+//         });
+       
+//         showMovies(movies);
+//     } catch (error) {
 //         console.error(error);
-//     });
+//     }
+// }
